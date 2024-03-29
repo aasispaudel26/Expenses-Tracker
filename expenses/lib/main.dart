2eb0b77f4b4dc1a4de113eec79e0fbf347c05217
Aasis <aasispaudel@26gmail.com>
2024-03-29 21:07:06 +0545

    second last part

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:expenses/widgets/expenses.dart';
 
+var kColorscheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 182));
+var kDarkColorscheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorscheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorscheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorscheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorscheme.onPrimaryContainer,
+          foregroundColor: kColorscheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorscheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorscheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                  fontSize: 16),
+            ),
+      ),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
